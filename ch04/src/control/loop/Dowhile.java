19d086552c6884090ae689afdd6319c104749556
KOITT <KOITT@KOITT-PC03>
2017-12-27 15:33:18 +0900

    java 4강 반복문 예제소스

---
+++ ch04/src/control/loop/Dowhile.java
@@ -0,0 +1,17 @@
+package control.loop;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+		do
+			System.out.print(i++ + " ");
+		
+		while (i <= 10);
+		
+		System.out.println();
+
+	}
+
+}
