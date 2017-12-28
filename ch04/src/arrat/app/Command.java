5b462b36d3e080e139860b6d0f37888bb920f3df
KOITT <KOITT@KOITT-PC03>
2017-12-28 12:51:54 +0900

    java 4강 배열 코드 저장

---
+++ ch04/src/arrat/app/Command.java
@@ -0,0 +1,13 @@
+package arrat.app;
+
+public class Command {
+
+	public static void main(String[] args) {
+		System.out.println("명령형 인자 갯수 >" + args.length);
+		
+		for (String value : args)
+			System.out.print(value + " ");
+
+	}
+
+}
