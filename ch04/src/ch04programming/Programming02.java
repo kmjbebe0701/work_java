136b3f05020ef6d5e81e0e6f1f129d00d40cd4cf
KOITT <KOITT@KOITT-PC03>
2017-12-28 10:31:41 +0900

    java 4강 Programming 문제

--- ch04/src/ch04programming/Programming02.java
+++ ch04/src/ch04programming/Programming02.java
@@ -3,8 +3,7 @@ package ch04programming;
 public class Programming02 {
 
 	public static void main(String[] args) {
-		
 
 	}
-
+	
 }
--- ch04/src/ch04programming/Programming03.java
+++ ch04/src/ch04programming/Programming03.java
@@ -1,9 +1,13 @@
 package ch04programming;
 
+import java.util.Scanner;
+
 public class Programming03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("정수를 입력해주세요");
+		int a = in.nextInt();
 
 	}
 
