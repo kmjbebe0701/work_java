d220c042d1a428b8d0415c8900b8eabec38b0b50
KOITT <KOITT@KOITT-PC03>
2017-12-29 14:59:47 +0900

    java 4강 강사님 제출 문제

--- ch04/src/exercise/Exercise02.java
+++ ch04/src/exercise/Exercise02.java
@@ -8,15 +8,33 @@ public class Exercise02 {
 		Scanner in = new Scanner(System.in);
 		System.out.println("정수를 입력하세요>>");
 		int num = in.nextInt();
-		int[][] a = new int[num][];
+
+		char[][] a = new char[num][num];
+		/*
+		 * int[][] a = new int[num][];
+		 * 
+		 * for (int i = 0; i < a.length; i++) { 
+		 * for (int j = 0; j <= i; j++) {
+		 * num[i][j] = '*'
+		 * System.out.print(num[i][j]
+		 * } 
+		 * System.out.println();
+		 */
 
 		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j <= i; j++) {
-				System.out.print('*');
+			for (int j = 0; j < a[i].length; j++) {
+				if (i >= j) {
+					a[i][j] = '*';
+				}
+
+			}
+		}
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j]);
+
 			}
 			System.out.println();
-			
-
 		}
 		in.close();
 
