c97eaa734d3687331079a9df4fc981ca04c7e69d
KOITT <KOITT@KOITT-PC03>
2018-01-09 15:48:04 +0900

    java 7장 클래스 만들기 추가

--- ch07/src/com/koitt/java/ch07/ArrayTest.java
+++ ch07/src/com/koitt/java/ch07/ArrayTest.java
@@ -4,9 +4,9 @@ public class ArrayTest {
 	
 	private Integer[] array;			//데이터를 담는 자료 구조 (참조형의 기본값은 null값)
 	private int length;					//배열의 크기
-	private int size; 				//실제 데이터의 개수
+	private int size; 					//실제 데이터의 개수
 	
-	public ArrayTest( int size) {
+	public ArrayTest(int size) {
 		this.length = size;
 		this.array = new Integer[this.length];
 	}
@@ -15,7 +15,7 @@ public class ArrayTest {
 	public void add(int index, int value) {
 		if (index >=0 && index < this.length) {
 			if(this.array[index] == null) {
-				this.array[index] =value;
+				this.array[index] = value;
 			}
 			else if (this.array[index] != null) {
 				
@@ -30,10 +30,6 @@ public class ArrayTest {
 					System.arraycopy(tempArray, 0, this.array, 0, tempArray.length);
 				}
 				
-				//집어 넣으려는 index 번호 앞쪽 복사
-				if (index == 0) {
-					this.array[index] = value;		//새로 추가하려는 값을 배열에 저장
-				}
 			
 				//집어 넣으려는 index 번호 뒤쪽 데이터를 한칸씩 뒤로 이동하여 복사
 				System.arraycopy(this.array, index, this.array, index+1, size - index);
@@ -46,11 +42,27 @@ public class ArrayTest {
 	}
 	
 	public void remove(int index) {
-		if (index >=0 &&index < this.length) {
+		if (index >=0 && index < this.length) {
+			if(this.array[index] == null) {
+				
+			}
+			else if (this.array[index] != null) {
+				
+				//집어 넣으려는 index 번호 뒤쪽 데이터를 한칸씩 앞으로 이동하여 복사
+				System.arraycopy(this.array, index+1, this.array, index, size - index);
+				this.array[this.array.length-1] = null;			//마지막 값을 null로 저장
+				this.size -=1;					//기존 데이터 개수 - 새로운 데이터 개수
+			
+			}
+			
+
+			
+			
 		}
 	}
 	
 	public void set (int index, int value) {
+		this.array[index] = value;
 		
 	}
 	public Integer get(int index) {
@@ -85,6 +97,48 @@ public class ArrayTest {
 		System.out.println();
 		
 		test.add(1, 1);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.remove(3);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.remove(1);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.remove(2);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.add(1, 8);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.add(2, 0);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.add(0, 7);
+		for (int i = 0; i <test.getSize(); i++) {
+			System.out.print(test.get(i));
+		}
+		System.out.println();
+		
+		test.set(0, 3);
 		for (int i = 0; i <test.getSize(); i++) {
 			System.out.print(test.get(i));
 		}
