823b8ed3fb07e573cec8fbbcad97d8dfabb18cf0
KOITT <KOITT@KOITT-PC03>
2018-01-11 10:44:57 +0900

    java 7장 Programming 문제

--- ch07/src/programming/Programming06.java
+++ ch07/src/programming/Programming06.java
@@ -6,14 +6,30 @@ public class Programming06 {
 	
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
-		String [][]s;
+		
 		cal.set(2018,(01-1),01);
+
 		System.out.println(cal.getTime());
-		System.out.println("2018년 1월 달력");
+		System.out.printf("   2018년 1월 달력    %n");
+		System.out.printf(" 일   월   화   수   목   금   토  %n");
 		
-		int a = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH);
+		
+		//int a = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH);
 		
 		
+		for (int i = 1, a = 1;a<=31;i++) {
+			if (i < cal.get(Calendar.DAY_OF_WEEK)) {
+				System.out.printf("   ");
+				continue;
+			}
+			if (i % 7 ==0) {
+				System.out.printf("%2d%n", a);
+				++a;
+				continue;
+			}
+			System.out.printf("%2d ", a);
+			++a;
+		}
 		
  		
 		
--- ch07/src/programming/Programming07.java
+++ ch07/src/programming/Programming07.java
@@ -11,22 +11,41 @@ public class Programming07 {
 		//Set<String> set = new HashSet<>();
 		HashSet<String> setA = new HashSet<>();
 		HashSet<String> setB = new HashSet<>();
-		StringTokenizer st = new StringTokenizer(str);
+		String[] ab = new String[100];
+		String str;
+		
 		
 		System.out.println("표준입역으로 문자열을 여러 줄에 입력하세요.");
 		System.out.println("결과를 보시려면 마지막에 표준입력으로 quit를 입력하세요");
+		
+		
+		for (int i = 0; i <100; i++) {
+			if (in.next().equals("quit")) {
+				System.out.println("quit");
+				break;
+			}
+			setA.add(in.next());
+		}
+		
+		System.out.println(setA.toString());
 
-		for (int i =0; setA.contains("quit"); i++) {
-		setA.add(in.next());
-		}
-		if(setA.contains("quit")) {
-			in.close();
-		}
+		/*System.out.println(str);
+		StringTokenizer st1 = new StringTokenizer(str);*/
 		
 		System.out.println(" 중복되지 않은 문자열 => " + setA.toString());
 		System.out.println(" 중복되지 않은 문자열 => " + setB.toString());
 		
 		
 	}
+	
+	public static void printToken(StringTokenizer st) {
+		System.out.println("토큰 수: " + st.countTokens());
+		int cnt = 0;
+		while (st.hasMoreTokens()) {
+			System.out.print(++cnt + ". " + st.nextToken() + " ");
+		}
+		System.out.println();
+	}
+	
 
 }
